0bfc76d2c27c5b3b5973105c5db1bee501af2058
Rafal Kwiatkowski <rafalkwiatkowski8@gmail.com>
2019-08-29 19:05:05 +0200

    Correct js file

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ var fs = require('fs');
 fs.readFile('./index.html', 'UTF-8', function(err, data) {
     var server = http.createServer();
     server.on('request', function(request, response) {
-        response.setHeader('Content-Type', 'text/html; charset=UTF-8;');
+        response.setHeader('Content-Type', 'text/html; charset=UTF-8');
         
         if(request.method === 'GET' && request.url === '/map') {
             response.write(data);
             response.end();
         } else {
             
-            fs.readFile('./errorpicture.jpg', 'UTF-8', function(err, data){
-                
-            response.statusCode = 404;
-            response.write('<h1>Error 404</h1>' + data);
-            response.end();
-            })
+            fs.readFile('./errorpicture.jpg', 'binary', function(err, data){
+                response.writeHead(200, {'Content-Type': 'image/jpg'});
+
+                response.write(data, 'binary')    
+                response.statusCode = 404;
+                response.end();
+            });
         }
     });
     
